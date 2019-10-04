fb7de734388cf318ddd1c97d773aa4c2858c2928
Alessandra <alessandraamontanher@gmail.com>
2019-10-03 22:31:56 -0300

    adding new routes, new pages for dashboard and login

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express'); // biblioteca - dependencia
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://aamontanher:aamontanher@cluster0-shard-00-00-yludg.m
 //permitir apenas localhost app.use(cors({origin:'http://localhost:3333'}));
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes); // precisa vir depois do express
 
 app.listen(3333);
